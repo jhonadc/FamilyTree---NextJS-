9c8359d632224461ec6da5d46664e1459de6bd58
jhonadc <jhonathanaugusto@gmail.com>
2022-07-20 16:26:50 +0200

    backup

--- pages/albums.js
+++ pages/albums.js
@@ -1,1 +1,2 @@
 import prisma from '../lib/prisma';
+
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@
 
 export default function Home() {
   return (
-    <div className='relative bg-indigo-800'>
+    <div className='relative bg-blue-800 max-h-max'>
       <div className='absolute inset-0'>
         <img
           className='w-full h-full object-cover'
@@ -33,8 +33,11 @@ export default function Home() {
       </div>
       <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
         <h1 className='text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl'>
-          Get in touch
+          Share your memories
         </h1>
+        <h3 className='text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl'>
+          ... for generations.
+        </h3>
         <p className='mt-6 text-xl text-indigo-100 max-w-3xl'>
           Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque
           lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut
