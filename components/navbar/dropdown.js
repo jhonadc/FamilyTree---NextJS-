91310b0fadb055f7c93152fc859001b894395cb8
jhonadc <jhonathanaugusto@gmail.com>
2022-08-09 13:20:54 +0200

    edit signed in menu to link dashboard

--- components/navbar/dropdown.js
+++ components/navbar/dropdown.js
@@ -39,12 +39,12 @@ export default function Dropdown() {
             <Menu.Item>
               {({ active }) => (
                 <a
-                  href='#'
+                  href='/dashboard'
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
                   )}>
-                  Account settings
+                  Dashboard
                 </a>
               )}
             </Menu.Item>
