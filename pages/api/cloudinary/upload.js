b166ce737efdaa054b1f962006d6ab3def8587b3
jhonadc <jhonathanaugusto@gmail.com>
2022-10-27 19:02:02 +0200

    api for cloudinary upload'

---
+++ pages/api/cloudinary/upload.js
@@ -0,0 +1,25 @@
+import { getImage } from '../../../utils/formidable';
+import { uploadImage } from '../../../cloudinaryutils/cloudinary';
+import prisma from '../../../prisma';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handle(req, res) {
+  const imageUploaded = await getImage(req);
+
+  const imageData = await uploadImage(imageUploaded.path);
+
+  const result = await prisma.image.create({
+    data: {
+      publicId: imageData.public_id,
+      format: imageData.format,
+      version: imageData.version.toString(),
+    },
+  });
+
+  res.json(result);
+}
