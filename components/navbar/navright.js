a78809aeb551f0b8a53f15eadf9c5ff16f3b4824
jhonadc <jhonathanaugusto@gmail.com>
2022-10-31 17:29:24 +0100

    removed signup button from navbar navright component

--- components/navbar/navright.js
+++ components/navbar/navright.js
@@ -19,15 +19,10 @@ export default function Navright() {
   } else {
     return (
       <>
-        <Link href='/register'>
-          <button className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-indigo bg-white-600 hover:bg-indigo-700 hover:text-white'>
-            Sign up
-          </button>
-        </Link>
         <button
           className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
           onClick={() => signIn()}>
-          Sign in
+          Enter
         </button>
       </>
     );
