f43695343e6e01a0eee958388d974f9789845a52
jhonadc <jhonathanaugusto@gmail.com>
2022-10-31 18:41:28 +0100

    logo link correct redirecting to home page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -23,7 +23,7 @@ export default function Navbar() {
       <Popover className='relative bg-white'>
         <div className='flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8'>
           <div className='flex justify-start lg:w-0 lg:flex-1'>
-            <a href='/home' className='inline'>
+            <a href='/' className='inline'>
               <span className='sr-only'>Family Tree</span>
               <img
                 className='h-8 w-auto sm:h-10'
