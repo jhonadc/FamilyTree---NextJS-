5412b99e282d93b42c34a6e1270c0ea17721033c
jhonadc <jhonathanaugusto@gmail.com>
2022-11-08 16:35:10 +0100

    SECURITY headers implemented

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(); battery=(self); geolocation=(); microphone=()',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+        ],
+      },
+    ];
+  },
   future: {
     webpack5: true,
   },
