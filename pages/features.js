cfe7df21d87f04fcb456996a02736dad016d1232
jhonadc <jhonathanaugusto@gmail.com>
2022-11-14 19:27:33 +0100

    gradient bg on about page. color adjustments

--- pages/features.js
+++ pages/features.js
@@ -42,13 +42,13 @@ const features = [
 
 export default function Features() {
   return (
-    <div className='bg-white'>
+    <div className='bg-gradient-to-r from-blue-600 to-blue-200 '>
       <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8'>
         <div className='max-w-3xl mx-auto text-center'>
-          <h2 className='text-5xl font-extrabold text-blue-900'>
+          <h2 className='text-5xl font-extrabold text-white'>
             Perks of the Family Tree Platform
           </h2>
-          <p className='mb-20 mt-4 text-lg text-gray-500'>
+          <p className='mb-20 mt-4 text-lg text-gray-900'>
             An ever scaling application to store and shares your memories and
             data, like DNA (when possible).
           </p>
@@ -58,14 +58,14 @@ export default function Features() {
             <div key={feature.name} className='relative'>
               <dt>
                 <CheckIcon
-                  className='absolute h-6 w-6 text-blue-500'
+                  className='absolute h-6 w-6 text-white'
                   aria-hidden='true'
                 />
-                <p className='ml-9 text-lg leading-6 font-medium text-gray-900'>
+                <p className='ml-9 text-lg leading-6 text-strong font-medium text-black'>
                   {feature.name}
                 </p>
               </dt>
-              <dd className='mt-2 ml-9 text-base text-gray-500 mb-10'>
+              <dd className='mt-2 ml-9 text-base text-gray-900 mb-10'>
                 {feature.description}
               </dd>
             </div>
