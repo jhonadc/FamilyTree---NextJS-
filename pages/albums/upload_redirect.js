b11739d71c43e09388987db6a9f6f4933b5e40e7
jhonadc <jhonathanaugusto@gmail.com>
2022-11-16 16:05:42 +0100

    deleted redirect api

--- pages/albums/upload_redirect.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    // some await stuff here
-    res.redirect(307, '/welcome');
-  } catch (err) {
-    res.status(500).send({ error: 'Error while fetching data' });
-  }
-}
