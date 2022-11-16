28a5d9e2612ba5a87f80359257702bdc12bdcfda
jhonadc <jhonathanaugusto@gmail.com>
2022-11-16 11:26:03 +0100

    main image hides on small displays

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
     <>
       <div className='hrelative bg-black'>
         <div className='mx-auto my-5 py-20  px-6 lg:px-40 text-justify content-center'>
-          <h1 className='antialiased font-extrabold tracking-tight text-white text-4xl lg:text-6xl'>
+          <h1 className='antialiased font-extrabold tracking-tight text-white text-3xl  lg:text-6xl'>
             Share your memories
           </h1>
-          <h3 className='mt-4 antialiased text-blue-500 lg:text-6xl text-xl font-extrabold tracking-tight text-5xl lg:text-6xl text-center'>
+          <h3 className='mt-4 antialiased text-blue-500 text-4xl lg:text-4xl font-extrabold tracking-tight text-center'>
             ... for GENERATIONS
           </h3>
 
-          <p className=' mt-10 antialiased leading-8 text-xl md:text-2xl lg: text-4xl text-indigo-100  text-justify m-5'>
+          <p className=' mt-10 antialiased leading-8 text-lg md:text-2xl text-indigo-100  text-justify m-5'>
             <span className='underline decoration-blue-500 '>Family Tree </span>
             allows family members to safely share, via the internet and cloud
             storage, digitalized memories (photos, texts, small videos) with
@@ -25,8 +25,11 @@ export default function Home() {
   </button> */}
         </div>
 
-        <div className=''>
+        <div className='max-lg:hidden'>
           <img className='' src='/mainpage.png' alt='' />{' '}
+        </div>
+        <div className='xl:hidden'>
+          <img className='' src='/mainphoto1.png' alt='' />{' '}
         </div>
       </div>
     </>
