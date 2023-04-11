1cd773e3b6cf6e69cf68a353df4efad9036467ae
jhonadc <jhonathanaugusto@gmail.com>
2023-04-11 15:32:18 +0200

    trying to fix error  on app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,15 @@ export default function App({
 
     <SessionProvider session={pageProps.session}>
       <Head>
+        <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q91LKR3HT0"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+
           gtag('config', 'G-Q91LKR3HT0');
-          </script>
+        </script>
       </Head>
       <Layout>
         <Component {...pageProps} />
