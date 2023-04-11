8bfceb6862cee25dcc10002ab6b60894beb3a642
jhonadc <jhonathanaugusto@gmail.com>
2023-04-11 14:09:03 +0200

    added google analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import Layout from '../components/layout';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 export default function App({
   Component,
@@ -9,6 +10,17 @@ export default function App({
 }) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q91LKR3HT0"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Q91LKR3HT0');
+        </script>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
