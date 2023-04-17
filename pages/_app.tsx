0740cb35abc988e5575313b4eeae2fcf467cab77
jhonadc <jhonathanaugusto@gmail.com>
2023-04-17 15:49:59 +0200

    ga installed and configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,48 @@ import 'tailwindcss/tailwind.css';
 import Layout from '../components/layout';
 import { SessionProvider } from 'next-auth/react';
 import Script from 'next/script';
-import { GoogleAnalytics } from "nextjs-google-analytics";
-
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
-    <GoogleAnalytics trackPageViews />
+    <div className="container">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q91LKR3HT0"
+        strategy="afterInteractive"
+      />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Q91LKR3HT0', {
+            page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+    </div>
     <SessionProvider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
